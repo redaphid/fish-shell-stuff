4a7d1d0bd34bc673358712c7287ce4eec8f2a519
redaphid <iam@aaronherres.com>
2021-03-21 13:58:22 -0700

    fancier checksum

--- bitcoin-address-checksum.fish
+++ bitcoin-address-checksum.fish
@@ -1,6 +1,12 @@
-# Defined in /tmp/fish.hlqcdw/bitcoin-address-checksum.fish @ line 2
+# Defined in /tmp/fish.itZ79r/bitcoin-address-checksum.fish @ line 2
 function bitcoin-address-checksum --description 'verifies that the string is a bitcoin address' --argument addr
-	decho "Key: " $addr
+	decho "~"
+	decho
+
+	decho "Step 0: Address:" 
+	decho $addr
+	decho
+
 	set addr_hex (echo $addr | base58 -d | base16 | string upper)
 	set -q addr_hex[1]; or begin
 		echo hex of $addr is invalid \($addr_hex\)
@@ -9,19 +15,21 @@ function bitcoin-address-checksum --description 'verifies that the string is a b
 
 	decho "Step 1: Hex ✓"
 	decho $addr_hex
+	decho
+
 	decho "Step 2: Check first byte "
-	test (string sub --start 1 --length 2 $addr_hex) = '80'
-	and decho "✓"
-	or begin; decho \n "...address doesn't begin with 80"; return 1; end
+	set prefix (string sub --start 1 --length 2 $addr_hex)
+	decho $prefix
+	decho '80'
+
+	test $prefix = '80'; or return 1
 	decho
 	
 	decho "Step 3: extract checksum ✓"
 	set chk1 (string sub --start -8 $addr_hex)
-	decho $chk1
 	set addr_new_len (math (string length $addr_hex) - 8)
 	set addr_no_chk (string sub --length $addr_new_len $addr_hex)
-	decho "address without checksum:" 
-	decho $addr_no_chk
+	decho $chk1
 	decho
 
 	decho "Step 4: sha256 hash of 3 ✓"
@@ -30,20 +38,24 @@ function bitcoin-address-checksum --description 'verifies that the string is a b
 	decho
 	
 	decho "Step 5: sha256 hash of 4 ✓"
-	set addr_hash_2 (string split ' ' (echo $addr_hash_1 | base16 -d | sha256sum | string upper))[1]
+	set addr_hash_2 (echo $addr_hash_1 | base16 -d | sha256sum | string upper)
 	decho $addr_hash_2
-	set chk2 (string sub --length 8 $addr_hash_2)
+
+	set chk2 (string sub --start 1 --length 8 $addr_hash_2)
+	decho
+
+	decho "Step 6: compare checksums"
+	decho $chk1
 	decho $chk2
 	decho
 
-	decho -n "Step 6: compare checksums "
-	test $chk1 = $chk2; or begin
-		decho "checksums didn't match :("
-		return 1
-	end
-
+	test $chk1 = $chk2; or return 1
+	decho "Step 7: We're good!"
 	echo $addr
-	decho "✓"
+	decho
+	
+	decho "~"
+	decho
 
 	return 0
 end
