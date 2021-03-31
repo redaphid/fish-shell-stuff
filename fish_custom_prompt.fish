363292e4bba3af76b3c19468436dada493da0b04
redaphid <iam@aaronherres.com>
2021-03-31 14:09:18 -0700

    add username

--- fish_custom_prompt.fish
+++ fish_custom_prompt.fish
@@ -1,4 +1,4 @@
 # Defined in /tmp/fish.V6vvL4/fish_custom_prompt.fish @ line 2
 function fish_custom_prompt
-    hostname_color_text; echo (hostname)
+    hostname_color_text; echo (whoami)@(hostname)
 end
