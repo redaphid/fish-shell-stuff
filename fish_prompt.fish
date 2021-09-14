57766b154a40007353b145acd6a7cc0548c72899
pull.rebase <iam@aaronherres.com>
2021-09-14 16:12:58 -0700

    hopefully tide doesn't mess things up

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,51 +1,38 @@
-# Defined in /tmp/fish.TZJWgM/fish_prompt.fish @ line 2
 function fish_prompt
-  set -l last_command_status $status
-  set -l cwd
+end
 
-  if test "$theme_short_path" = 'yes'
-    set cwd (basename (prompt_pwd))
-  else
-    set cwd (prompt_pwd)
-  end
+status is-interactive || exit
 
-  set -l fish     (type -q fish_custom_prompt; and fish_custom_prompt; or echo "⋊>")
-  set -l ahead    "↑"
-  set -l behind   "↓"
-  set -l diverged "⥄ "
-  set -l dirty    "⨯"
-  set -l none     "◦"
+_tide_remove_unusable_items
 
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
-  set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
+# The first element in $$_tide_prompt_var is right prompt
+# All remaining ones are 'left' prompt (also upper right in 2-line prompts)
+set -g _tide_prompt_var _tide_prompt_$fish_pid
 
-  if test $last_command_status -eq 0
-    echo -n -s $success_color $fish $normal_color
-  else
-    echo -n -s $error_color $fish $normal_color
-  end
+function _tide_refresh_prompt --on-variable $_tide_prompt_var
+    set -g _tide_self_repainting # prevents us from creating a second background job
+    commandline --function repaint
+end
 
-  if git_is_repo
-    if test "$theme_short_path" = 'yes'
-      set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
-      set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+function fish_prompt
+    _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
+        jobs --query
+        fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
+            fish_bind_mode=$fish_bind_mode set -U $_tide_prompt_var (_tide_prompt)" &
+        builtin disown
+
+        command kill $_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid $last_pid
     end
 
-    echo -n -s " " $directory_color $cwd $normal_color
-    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
+    test "$tide_prompt_add_newline_before" = true && echo
+    string unescape $$_tide_prompt_var[1][2..]
+end
 
-    if git_is_touched
-      echo -n -s $dirty
-    else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
-    end
-  else
-    echo -n -s " " $directory_color $cwd $normal_color
-  end
+function fish_right_prompt
+    string unescape $$_tide_prompt_var[1][1]
+end
 
-  echo -n -s " "
+function _tide_on_fish_exit --on-event fish_exit
+    set -e $_tide_prompt_var
 end
