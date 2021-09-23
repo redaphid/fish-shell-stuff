cba146f3c424902f17fc2b65ee0130d6f38ff918
redaphid <iam@aaronherres.com>
2021-09-23 01:08:40 -0700

    make it executable

--- if-internet-is-too-slow-restart.fish
+++ if-internet-is-too-slow-restart.fish
@@ -1,9 +1,14 @@
+#!/usr/bin/env fish
 function if-internet-is-too-slow-restart
     set -l minimum_speed 200
-    set -l raw_internet_speed (cat tmp/speedtest.json | jq '.download')
+    set -l raw_internet_speed (speedtest --json | jq '.download')
     set -l internet_speed (math $raw_internet_speed '/' 1000000)
     echo $internet_speed $minimum_speed $raw_internet_speed
     test $internet_speed -gt $minimum_speed; or begin
-      echo "too slow"
+      echo "I'm gonna do it"
+      sudo /usr/sbin/ethtool -s (network-names) speed 1000 duplex full autoneg off
+
     end
+    touch /tmp/too-slow-(date)
 end
+status is-interactive; or if-internet-is-too-slow-restart
