f264d2943e8b727e52c17ba00b276193e40adeea
redaphid <iam@aaronherres.com>
2021-09-28 16:05:11 -0700

    use the lastest version of my mega gitignore

--- gitignore-get.fish
+++ gitignore-get.fish
@@ -1,3 +1,3 @@
-function get-gitignore
+function gitignore-get
 curl https://gist.githubusercontent.com/redaphid/a0617db69f81d45fa9c62cf3f6995cbf/raw/0847c75152cd39b1cf9fe80f073aea242090e536/.gitignore
 end
