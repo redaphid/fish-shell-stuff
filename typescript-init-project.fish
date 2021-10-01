60886183238ead01e5b1068728e51b3873d56ade
redaphid <iam@aaronherres.com>
2021-09-30 22:49:32 -0700

    typescript initter

---
+++ typescript-init-project.fish
@@ -0,0 +1,19 @@
+function typescript-init-project
+#!/usr/bin/env fish
+yarn set version berry
+yarn init --workspace
+yarn add --dev jest typescript
+yarn add --dev ts-jest @types/jest
+yarn ts-jest config:init
+yarn dlx @yarnpkg/sdks vscode
+npx npm-add-script -k "test" -v "jest"
+mkdir src
+touch src/index.ts
+touch src/index.test.ts
+echo "
+  test(\"Let's get things started\", ()=>{
+    expect(true).toBe(false)
+  })
+" > src/index.test.ts
+yarn test
+end
