8655e7f82eec52c947db54c404aab62db3a034a5
redaphid <iam@aaronherres.com>
2021-12-15 18:40:41 -0700

    accidentally block code quote in description md

--- git-protocol-labs-create-pr.fish
+++ git-protocol-labs-create-pr.fish
@@ -1,8 +1,21 @@
-# Defined in /home/redaphid/.config/fish/functions/git-protocol-labs-create-pr.fish @ line 2
+# Defined in /tmp/fish.Lx1efE/git-protocol-labs-create-pr.fish @ line 2
 function git-protocol-labs-create-pr --argument issue issue_type
 	set -q issue_type[1]; or set issue_type "feat"
 	set issue_title (gh issue view $issue --json title --jq '.title')
+	
 	set branch_name (string join '/' $issue_type (echo $issue_title | string lower | string replace -a ' ' '-'))
-	echo "branch_name" $branch_name
-	#git checkout -b (string join '/' "feat" (gh issue view $issue --json title --jq ".title" | string lower | string replace -a ' ' '-'))
+
+	git checkout -b $branch_name; or begin
+ 			echo "couldn't checkout $branch_name"
+			return
+		end
+
+	set commit_msg """
+                $issue_type:  $issue_title
+                Closes #$issue
+        """
+
+	git commit --allow-empty -m $commit_msg; and git push -u origin $branch_name
+
+	gh pr create --draft --base main --title "$issue_type: $issue_title"  --body $commit_msg
 end
