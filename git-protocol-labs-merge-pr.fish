d1a1848e7ddc6db14d85c33a2889dad0bf2a025a
redaphid <iam@aaronherres.com>
2021-12-15 19:35:33 -0700

    everything except body

--- git-protocol-labs-merge-pr.fish
+++ git-protocol-labs-merge-pr.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.A6Gq3V/git-protocol-labs-merge-pr.fish @ line 2
+# Defined in /tmp/fish.cY9yYy/git-protocol-labs-merge-pr.fish @ line 2
 function git-protocol-labs-merge-pr
 	set branch_name (git branch --show-current)
 	echo $branch_name
 	gh pr ready $branch_name
 	set commit_msg (gh pr view $branch_name --json "body" --jq ".body")
-	gh pr merge $branch_name --squash --body (string join '\n' $commit_msg)
+	gh pr merge $branch_name --squash
 end
