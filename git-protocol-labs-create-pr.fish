dd33bf75e6125bec23e19b0997ba3cab9d97fb81
redaphid <iam@aaronherres.com>
2021-12-15 18:37:20 -0700

    gettin close

--- git-protocol-labs-create-pr.fish
+++ git-protocol-labs-create-pr.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.RXljDT/git-protocol-labs-create-pr.fish @ line 2
+# Defined in /home/redaphid/.config/fish/functions/git-protocol-labs-create-pr.fish @ line 2
 function git-protocol-labs-create-pr --argument issue issue_type
 	set -q issue_type[1]; or set issue_type "feat"
 	set issue_title (gh issue view $issue --json title --jq '.title')
