5b7b1b262c3bab79c35e7a6f85f049e262e92c9d
redaphid <iam@aaronherres.com>
2021-12-15 19:25:55 -0700

    merge pr close. Except newlines

--- git-protocol-labs-merge-pr.fish
+++ git-protocol-labs-merge-pr.fish
@@ -1,5 +1,8 @@
-# Defined in /tmp/fish.NpGdoA/git-protocol-labs-merge-pr.fish @ line 2
+# Defined in /tmp/fish.A6Gq3V/git-protocol-labs-merge-pr.fish @ line 2
 function git-protocol-labs-merge-pr
-	set commit_msg (gh pr view --json "body" --jq ".body")
-	gh pr merge --squash --body $commit_msg
+	set branch_name (git branch --show-current)
+	echo $branch_name
+	gh pr ready $branch_name
+	set commit_msg (gh pr view $branch_name --json "body" --jq ".body")
+	gh pr merge $branch_name --squash --body (string join '\n' $commit_msg)
 end
