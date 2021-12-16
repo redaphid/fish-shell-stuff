207160d34b9c999bc5f078fb2971211821f95db1
redaphid <iam@aaronherres.com>
2021-12-15 18:13:54 -0700

    correct feature type prefix

--- git-protocol-labs-create-pr.fish
+++ git-protocol-labs-create-pr.fish
@@ -1,9 +1,8 @@
-# Defined in /tmp/fish.2NTIcy/git-protocol-labs-create-pr.fish @ line 2
+# Defined in /tmp/fish.Qzw5ND/git-protocol-labs-create-pr.fish @ line 2
 function git-protocol-labs-create-pr --argument issue type
-	set -q type[1]; or set type "feat"
+	set -q issue_type[1]; or set issue_type "feat"
 	set issue_title (gh issue view $issue --json title --jq '.title')
-	echo "title" $issue_title
-	set branch_name (string join '/' "feat" (echo $issue_title | string lower | string replace -a ' ' '-'))
+	set branch_name (string join '/' $issue_type (echo $issue_title | string lower | string replace -a ' ' '-'))
 	echo "branch_name" $branch_name
 	#git checkout -b (string join '/' "feat" (gh issue view $issue --json title --jq ".title" | string lower | string replace -a ' ' '-'))
 end
