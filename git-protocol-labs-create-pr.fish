8a5d076c0695e1c8af322b4d8188018486e8d051
redaphid <iam@aaronherres.com>
2021-12-15 18:12:25 -0700

    generate branch name, branch type

--- git-protocol-labs-create-pr.fish
+++ git-protocol-labs-create-pr.fish
@@ -1,6 +1,9 @@
-# Defined in /tmp/fish.cntXB0/git-protocol-labs-create-pr.fish @ line 2
-function git-protocol-labs-create-pr --argument issue
+# Defined in /tmp/fish.2NTIcy/git-protocol-labs-create-pr.fish @ line 2
+function git-protocol-labs-create-pr --argument issue type
+	set -q type[1]; or set type "feat"
 	set issue_title (gh issue view $issue --json title --jq '.title')
-	echo $issue_title
+	echo "title" $issue_title
+	set branch_name (string join '/' "feat" (echo $issue_title | string lower | string replace -a ' ' '-'))
+	echo "branch_name" $branch_name
 	#git checkout -b (string join '/' "feat" (gh issue view $issue --json title --jq ".title" | string lower | string replace -a ' ' '-'))
 end
