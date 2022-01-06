9970139d250a1c590ee6f0a5c63fe498035b23af
redaphid <iam@aaronherres.com>
2022-01-06 15:56:58 -0700

    gpo: for making sure you specify a remote

---
+++ gpo.fish
@@ -0,0 +1,8 @@
+function gpo --description 'Pushes current branch to the specified origin. Useful for pushing to multiple repos' --argument repo_name
+set -q repo_name[1]; or begin
+echo "Must specify a repo name!"
+echo "e.g. gpo redaphid"
+return
+end
+git push $repo_name (git branch --show-current)
+end
--- ipfs.fish
+++
@@ -1,3 +0,0 @@
-function ipfs --wraps=/home/redaphid/tools/go-ipfs/go-ipfs/ipfs
-docker exec ipfs_host ipfs $argv
-end
