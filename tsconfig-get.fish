7b5f4060b81697de8d3666a47ea878eafb01f1f1
redaphid <iam@aaronherres.com>
2022-01-07 20:52:15 -0700

    stop being verbose in curl

--- tsconfig-get.fish
+++ tsconfig-get.fish
@@ -1,3 +1,3 @@
 function tsconfig-get
-	curl -vvvv https://gist.githubusercontent.com/redaphid/c2c3e89444c7e7a183f4b48f94e46503/raw
+	curl https://gist.githubusercontent.com/redaphid/c2c3e89444c7e7a183f4b48f94e46503/raw
 end
