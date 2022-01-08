9791b3f3e7b5467fa37b1c5188fcbec720bb5d6d
redaphid <iam@aaronherres.com>
2022-01-07 21:12:33 -0700

    update project initter w/eslint, .gitignore without testing. Surely this won't cause problems

--- typescript-init-project.fish
+++ typescript-init-project.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.qovXBZ/typescript-init-project.fish @ line 2
+# Defined in /tmp/fish.7MvCDN/typescript-init-project.fish @ line 2
 function typescript-init-project
 #!/usr/bin/env fish
 yarn set version berry
-yarn init --workspace
+yarn init
 yarn add --dev jest typescript
 yarn add --dev ts-jest @types/jest
 yarn ts-jest config:init
@@ -10,6 +10,7 @@ yarn dlx @yarnpkg/sdks vscode
 npx npm-add-script -k "test" -v "jest"
 tsconfig-get > ./tsconfig.json
 gitignore-get > ./.gitignore
+eslintrc-get > ./.eslintrc
 mkdir src
 touch src/index.ts
 touch src/index.test.ts
