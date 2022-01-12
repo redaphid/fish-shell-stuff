0953143abac24c763c4b727f32a682541ba28015
redaphid <iam@aaronherres.com>
2022-01-12 15:26:11 -0700

    complain when you don't have pastel installed. Probably

--- hostname_color_text.fish
+++ hostname_color_text.fish
@@ -2,6 +2,10 @@
 function hostname_color_text
 	type -q pastel; or begin
 		echo "pastel isn't installed. no colors for you"
+		echo """you can install it like so:
+wget "https://github.com/sharkdp/pastel/releases/download/v0.8.1/pastel_0.8.1_amd64.deb"
+sudo dpkg -i pastel_0.8.1_amd64.deb"""
+return
 	end
 	set_color -b (hostname_color)
 	set_color (pastel format hex (pastel complement (hostname_color)))
