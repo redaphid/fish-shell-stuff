129d4c671fbe215b3c6ca3827bdb3f1a89ea087d
redaphid <iam@aaronherres.com>
2022-01-12 16:10:10 -0700

    at least have *some* script to setup the environment so the terminal doesn't crash

---
+++ redaphid_setup_fish.fish
@@ -0,0 +1,9 @@
+# Defined in /tmp/fish.4fEc2x/redaphid_setup_fish.fish @ line 1
+function redaphid_setup_fish
+    #omf
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+    #fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    #pastel
+    curl "https://github.com/sharkdp/pastel/releases/download/v0.8.1/pastel_0.8.1_amd64.deb" > /tmp/pastel.deb;and sudo apt install -y /tmp/pastel.deb; and rm /tmp/pastel.deb
+end
