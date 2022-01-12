4923c3d01c6c416e98d91b4801d2b71b9e2b3293
redaphid <iam@aaronherres.com>
2022-01-12 15:23:36 -0700

    slightly fix hostname color text problem when pastel isn't installed

--- hostname_color_text.fish
+++ hostname_color_text.fish
@@ -1,5 +1,8 @@
 # Defined in /home/redaphid/.config/fish/functions/hostname_color_text.fish @ line 2
 function hostname_color_text
+	type -q pastel; or begin
+		echo "pastel isn't installed. no colors for you"
+	end
 	set_color -b (hostname_color)
 	set_color (pastel format hex (pastel complement (hostname_color)))
 end
