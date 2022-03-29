d846f007e3bda34f63a45c7f90fb7e9662440900
redaphid <iam@aaronherres.com>
2022-03-29 13:22:13 -0700

    git-save pulls

---
+++ git-save.fish
@@ -0,0 +1,17 @@
+function git-save --description Adds\ all\ files\ in\ my\ notes\ project\ and\ pushes,\ without\ interrupting\ my\ workflow\ by\ cd\'ing\ and\ other\ steps --argument project message
+    set -q project[1]; or begin
+        echo "a project must be specified"
+        return -1
+    end
+    set -q message[1]; or set message "autosave"
+    pushd $project
+    git pull; or begin
+        echo "something bad happened while pulling"
+        popd
+        return -1
+    end
+    git add .
+    git commit -am $message
+    git push
+    popd
+end
