597e78aae6c0ad0c38e80f0a7e4b4b7c1de53894
redaphid <iam@aaronherres.com>
2022-04-15 16:24:20 -0700

    bak: check for file existence; don't bak already bak'd files. bak -u, otoh, unbaks the file until there's no '.bak' suffix remaining

--- bak.fish
+++ bak.fish
@@ -1,18 +1,29 @@
-# Defined in /tmp/fish.EGyAFd/bak.fish @ line 2
 function bak --description 'renames a <file | folder> to <file | folder>.bak . the -u flag undoes this'
-  argparse 'u/undo' -- $argv
+  argparse "u/undo" -- $argv
   set file $argv[1]
-
+  test -f "$file"; or begin
+    echo "$file doesn't exist. So I'm not gonna 'bak' it up"
+    return 1
+  end
   string length -q "$file"; or begin
     echo "filename not specified! usage: bak <file | folder>"
     return 1
   end
 
-  set new_file (string join '.' $file'.bak')
-
-  set -q _flag_undo; and begin;
-    set new_file (string replace -r '\.bak$' "" -- $file)
+  set -q _flag_undo; and begin
+    while string match -q "*.bak" "$file"
+      set new_file (string replace -r "\.bak\$" "" -- $file)
+      mv "$file" "$new_file"
+      set file "$new_file"
+    end
+    return 0
   end
-  
- mv $file $new_file
+
+  string match -q "*.bak" "$file"; and begin
+    echo "$file already has a .bak suffix. Cowardly refusing to do anything"
+    return 1
+  end
+
+  set new_file (string join "." "$file.bak")
+  mv "$file" "$new_file"
 end
