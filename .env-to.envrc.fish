87888c947bdb8a0941ed53411d347860bbb3c01c
redaphid <iam@aaronherres.com>
2022-05-05 11:47:34 -0700

    save

--- .env-to.envrc.fish
+++ .env-to.envrc.fish
@@ -1,6 +1,11 @@
 function .env-to.envrc --argument directory
     set -q directory[1]; or set directory $PWD
+    test -f $directory/.envrc; and begin
+        echo "$directory/.envrc exists. Cowardly refusing to do anything"
+        return
+    end
     for l in (cat $directory/.env)
-        string match -q '*=*' $l; and echo "$l has an equals sign"
+        string match -q '*=*' $l; or continue
+        set -a envrc "export $l"
     end
 end
