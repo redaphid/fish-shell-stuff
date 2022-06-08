022e21e474d79297a2533885fd6e0f19e0d852bf
redaphid <iam@aaronherres.com>
2022-06-08 12:57:03 -0700

    functions: disk-benchmark-get-results

--- disk-benchmark-get-results.fish
+++ disk-benchmark-get-results.fish
@@ -1,5 +1,5 @@
 function disk-benchmark-get-results --argument DIRECTORY
 	for f in (find $DIRECTORY | grep .json)		
-		cat $f | jq '.jobs | map({(.jobname):{read:.read.io_bytes, write:.write.io_bytes, rest: .}}) | add'
+		cat $f | jq '.jobs | map({(.jobname):{read:.read.io_bytes, write:.write.io_bytes}}) | add'
 	end
 end
