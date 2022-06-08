e3eda14178813c466fa4f7852eb725b5bc40aec1
redaphid <iam@aaronherres.com>
2022-06-08 11:45:39 -0700

    functions: ros-save

---
+++ ros-save.fish
@@ -0,0 +1,15 @@
+function ros-save
+ echo $argv
+ set -q dir[1]; or set dir "/home/redaphid/Projects/fish-shell-stuff"
+ set -q prefix[1]; or set prefix ''
+
+ pushd $dir
+
+ for f in $argv
+	funcsave --directory $dir $f
+	git add "$f.fish"
+ end
+ git commit -m "functions: $argv"
+ git push
+ popd
+end
