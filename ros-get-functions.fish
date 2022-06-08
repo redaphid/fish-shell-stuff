3083d2d25d7fb05ed060b592f52f7e728f42abae
redaphid <iam@aaronherres.com>
2022-06-08 12:14:08 -0700

    functions: ros-get-functions

---
+++ ros-get-functions.fish
@@ -0,0 +1,10 @@
+function ros-get-functions --argument quiet
+ for f in (functions)
+echo (functions -D $f) | grep -q 'redaphid'; or continue
+set fn_header (echo (functions $f)[2] | string replace -r '^function ' '')
+set -q quiet[1]; and begin
+set fn_header (string split ' ' $fn_header)[1]
+end
+echo $fn_header
+end
+end
