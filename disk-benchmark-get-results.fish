3364022dc457880dcb783a6342b4637f4982b7b9
redaphid <iam@aaronherres.com>
2022-06-08 14:09:36 -0700

    functions: disk-benchmark-get-results

--- disk-benchmark-get-results.fish
+++ disk-benchmark-get-results.fish
@@ -1,6 +1,6 @@
 function disk-benchmark-get-results --argument DIRECTORY
 	for f in (find $DIRECTORY | grep .json)				
-		set jq_output (cat $f | jq '.jobs[0] | {name: .jobname, read: .read.io_bytes, write: .write.io_bytes}')
+		set jq_output (cat $f | jq '.jobs[0] | {name: .jobname, read: .read.iops, write: .write.iops}')
 		echo "### $f ###"\n
 		echo "read:" (numfmt --to=si (echo $jq_output | jq -r '.read'))
 		echo "write:" (numfmt --to=si (echo $jq_output | jq -r '.write'))
