5092c1408758fac89c41f1492c49a9cb51cba12c
redaphid <iam@aaronherres.com>
2022-06-08 23:10:39 -0700

    functions: macro_watcher

---
+++ macro_watcher.fish
@@ -0,0 +1,13 @@
+function macro_watcher
+evtest /dev/input/event25 | while read line
+echo $line | grep -q "EV_KEY"; or continue
+echo $line | grep -q "time"; or continue
+ set parts (string split ' ' $line);
+ set state_raw $parts[-1]; 
+ test "0" -eq "$state_raw"; and set state "up"
+ test "1" -eq "$state_raw"; and set state "down"
+ test "2" -eq "$state_raw"; and set state "hold"
+ set key (string trim --chars="()," $parts[-3])
+ echo "key: $key state: $state"
+end
+end
