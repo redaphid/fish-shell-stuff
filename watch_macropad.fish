44ea8a508156fa3f9116ed30c616c51bc7ac00e5
redaphid <iam@aaronherres.com>
2022-06-08 22:53:26 -0700

    functions: watch_macropad

---
+++ watch_macropad.fish
@@ -0,0 +1,3 @@
+function watch_macropad
+evtest /dev/input/event25 | while read line; echo $line | grep -q "EV_KEY"; or continue; echo "got an event: $line"; set parts (string split ' ' $line)[-3]; echo $parts;end
+end
