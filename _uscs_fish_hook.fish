82b449d7565f400556f433ff34d5f22ae6bbd36a
redaphid <iam@aaronherres.com>
2022-06-10 01:20:12 -0700

    remove old uscs hooks that aren't necessary with new versions of fish

--- _uscs_fish_hook.fish
+++
@@ -1,8 +0,0 @@
-function _uscs_fish_hook --on-event fish_prompt
-    if not contains "$HOME/uscs/bin" $fish_user_paths                                                                                                                                            13:39:38
-            set -Ua fish_user_paths $HOME/uscs/bin                            
-    end    
-    for f in (ls ~/profile.d/*.fish)
-        . $f
-    end
-end
--- _uscs_fish_profile_loader.fish
+++
@@ -1,5 +0,0 @@
-function _uscs_fish_profile_loader
-    for f in (ls ~/profile.d/*.fish)
-        source $f
-    end
-end
