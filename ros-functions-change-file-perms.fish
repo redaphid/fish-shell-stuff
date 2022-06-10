08e9bf8bd17d5dcdf595219717122a585423815c
redaphid <iam@aaronherres.com>
2022-06-10 03:51:11 -0700

    functions: ros-functions-change-file-perms

---
+++ ros-functions-change-file-perms.fish
@@ -0,0 +1,9 @@
+function ros-functions-change-file-perms --argument directory
+for f in (ls -1 $directory)
+ set pre (cat $f)[1]
+test "#!/usr/bin/env fish" = $pre; or continue
+test -x $f; and continue
+echo "making $f executable";
+chmod +x $f;
+end
+end
