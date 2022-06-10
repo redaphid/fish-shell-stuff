92d307f0d7513a19861e54962866f5b12a19a106
redaphid <iam@aaronherres.com>
2022-06-10 04:06:02 -0700

    autosave

--- save-notes.fish
+++ save-notes.fish
@@ -1,3 +1,5 @@
+#!/usr/bin/env fish
 function save-notes --description 'a simple function to save my notes' --argument message
   git-save ~/Projects/notes $message;
 end
+status is-interactive; or save-notes $argv
