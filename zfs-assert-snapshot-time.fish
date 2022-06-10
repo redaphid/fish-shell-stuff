1cc605f109ad398a431ac58f894f8b133edc068b
redaphid <iam@aaronherres.com>
2022-06-10 00:56:37 -0700

    functions: zfs-assert-snapshot-time

--- zfs-assert-snapshot-time.fish
+++ zfs-assert-snapshot-time.fish
@@ -3,12 +3,12 @@ function zfs-assert-snapshot-time --argument dataset max_age
     set -q dataset[1]; or set dataset $ROS_ZFS_DATASET_TO_CHECK
     set -q dataset[1]; or begin
         echo "I need to know which dataset to check. or set the ROS_ZFS_DATASET_TO_CHECK env var"
-        return
+        return 1
     end
     set -q max_age[1]; or set max_age $ROS_ZFS_SNAPSHOT_MAX_AGE
     set -q max_age[1]; or begin
         echo "I need to know the max age this dataset can be; either run this function via `zfs-assert-snapshot-time <dataset> <max_age> or set the env var ROS_ZFS_SNAPSHOT_MAX_AGE"
-        return
+        return 1
     end
     set now_epoch (date +"%s")
     set -e snapshot_times
