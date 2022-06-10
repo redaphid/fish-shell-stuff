adaf633306238177a1fbd16c527bbf07b9862cc0
redaphid <iam@aaronherres.com>
2022-06-10 01:44:34 -0700

    functions: ros-functions-prune

---
+++ ros-functions-prune.fish
@@ -0,0 +1,10 @@
+function ros-functions-prune --description remove\ functions\ that\ don\'t\ exist\ in\ this\ shell\ from\ the\ ROS_SAVED_FUNCTIONS\ list
+    set -l VERIFIED_FUNCTIONS ''
+    for f in $ROS_SAVED_FUNCTIONS
+        functions -q $f; or continue
+        set -a VERIFIED_FUNCTIONS $f
+
+    end
+
+    set ROS_SAVED_FUNCTIONS (string split ' ' $VERIFIED_FUNCTIONS[2..] | sort | uniq)
+end
