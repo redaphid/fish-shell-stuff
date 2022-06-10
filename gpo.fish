4b4910091c49946be651c71bc67c93d991597bb0
redaphid <iam@aaronherres.com>
2022-06-10 04:03:59 -0700

    make em all executable

--- gpo.fish
+++ gpo.fish
@@ -1,3 +1,4 @@
+#!/usr/bin/env fish
 function gpo --description 'Pushes current branch to the specified origin. Useful for pushing to multiple repos' --argument repo_name
 set -q repo_name[1]; or begin
 echo "Must specify a repo name!"
@@ -6,3 +7,4 @@ return
 end
 git push $repo_name (git branch --show-current)
 end
+status is-interactive; or gpo $argv
