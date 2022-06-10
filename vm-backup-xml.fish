b493b0fae04b691117acc993373626f39c338b02
redaphid <iam@aaronherres.com>
2022-06-09 23:42:50 -0700

    functions: vm-backup-xml

---
+++ vm-backup-xml.fish
@@ -0,0 +1,21 @@
+function vm-backup-xml --argument backup_directory
+	set -q backup_directory[1]; or set backup_directory $ROS_VM_LOCATION
+	set -q backup_directory[1]; or begin
+		echo "you must specify a directory to back the xml in to"
+		echo "usage: vm-backup-xml <directory>"
+		echo "or set the env variable ROS_VM_LOCATION"
+		return
+	end	
+	set vms (virsh list --all --name | grep [a-zA-Z])
+	for vm in $vms
+		virsh dumpxml $vm > $backup_directory/$vm.xml
+	end
+
+	set xml_file_count (ls -1 $backup_directory/*.xml | wc -l)
+	set vm_count (echo $vms | wc -l)
+	test $xml_file_count -ge $vm_count; or begin
+		echo "this script didn't export everything. Ugh $xml_file_count $vm_count"
+		return
+	end
+	
+end
