0741da869309bfea61edf70d6f490c9a0f83ab6e
redaphid <iam@aaronherres.com>
2022-06-10 02:22:54 -0700

    functions: ros-functions-make-executable

--- ros-functions-make-executable.fish
+++ ros-functions-make-executable.fish
@@ -7,16 +7,21 @@ function ros-functions-make-executable --argument directory
     set -l shebang "#!/usr/bin/env fish"
     for f in (find $ROS_FUNCTION_LOCATION | grep '.fish$')
         test -x $f; or continue
-        echo (basename $f)
         set -l fn_name (string replace --regex '\.fish$' '' (basename $f))
-        echo $fn_name; and return
         set -l body (cat $f)
         string match -q $shebang $body[1]; or begin
             echo "$f has the wrong shebang" $body[1]
             set -p body $shebang
-            for l in $body
-                echo $l
+        end
+        set good_footer "status is-interactive; or $fn_name \$argv"
+        set fn_footer $body[-1]
+        string match -q $good_footer $fn_footer; or begin
+            string match -rq "end|return" $fn_footer; or begin
+                echo "Not sure what the end of this function is, but I don't want to risk anything. Aborting."
             end
+            echo "we can work with $fn_footer"
+            set -a body $good_footer
+
         end
     end
 end
