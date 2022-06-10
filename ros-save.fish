903f6d6f697d12c025735124cd156b08b1840754
redaphid <iam@aaronherres.com>
2022-06-10 00:58:23 -0700

    functions: ros-save

--- ros-save.fish
+++ ros-save.fish
@@ -1,13 +1,15 @@
 function ros-save
-    echo $argv
     set -q save_dir[1]; or set save_dir $ROS_FUNCTION_LOCATION
     set -q save_dir[1]; or begin
         echo "I need to know where to save stuff to. try calling this with `save_dir=<directory with the functions> ros-save <functions...>"
         echo "or setting ROS_FUNCTION_LOCATION as en env var for your system"
         return 1
     end
-    set -q argv[1]; or begin
+    set fns $argv
+    set -q fns[1]; or set fns $ROS_SAVE_FUNCTIONS
+    set -q fns[1]; or begin
         echo "pls specify the functions you wish to save. e.g. `ros-save hello_world`"
+        echo "or specify ROS_SAVE_FUNCTIONS in the environment somewhere"
         return 1
     end
     set -q prefix[1]; or set prefix ''
