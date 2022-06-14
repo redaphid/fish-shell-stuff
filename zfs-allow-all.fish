0358c53acc82b3e37a657e61e451a885a7d86813
knock <knock@knock.local>
2022-06-14 16:28:39 +0000

    functions: zfs-allow-all

--- zfs-allow-all.fish
+++ zfs-allow-all.fish
@@ -8,8 +8,12 @@ function zfs-allow-all --description 'allows <user> all permissions to <dataset>
     for line in (zfs allow 2>&1)
 	echo $line | grep -q 'NAME'; and set found_beginning "true"
 	set -q found_beginning[1]; or continue
-	string match -r '^y' $dry_run[1]; and echo "Dry run"
 	echo $line | grep -q '[[:space:]]\(subcommand\|property\)[[:space:]]'; or continue
+        set perm (string split --no-empty ' ' $line)[1]
+	string match -q -r '^y' $dry_run[1]; and begin 
+		echo zfs allow -r -u $user $perm $dataset
+		continue
+	end
 	set perm (string split --no-empty ' ' $line)[1]
 	echo zfs allow -r -u $user $perm $dataset
     end
