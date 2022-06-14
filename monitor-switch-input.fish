c08797828a53cd689448112398fd8d112bec8caa
redaphid <iam@aaronherres.com>
2022-06-14 11:48:42 -0700

    functions: monitor-switch-input

--- monitor-switch-input.fish
+++ monitor-switch-input.fish
@@ -2,9 +2,7 @@ function monitor-switch-input --argument monitor
     set -q monitor[1]; or set -l monitor $MONITOR_INPUT
     while true
         sleep 3
-	echo "monitor before: $monitor"
 	set -q monitor[1]; or set -l monitor (monitor-get-control-device)
-	echo "monitor after: $monitor"
         has_das; and begin
             echo "has das"
             ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep 15; and continue
