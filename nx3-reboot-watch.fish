40275168886927a49abace15147da63d07e4efc7
redaphid <iam@aaronherres.com>
2022-06-13 19:37:04 -0700

    functions: nx3-reboot-watch

--- nx3-reboot-watch.fish
+++ nx3-reboot-watch.fish
@@ -1,12 +1,12 @@
 function nx3-reboot-watch
- 
-while true
-sleep 5
-set NX3_NEW_CODE (curl "$NX3_REBOOT_URL?code=$NX3_QUERY_CODE" 2>/dev/null)
-test "$NX3_OLD_CODE" = "$NX3_NEW_CODE"; or begin;
-echo "rebooting nx3"
-virsh reboot nx3
-set NX3_OLD_CODE $NX3_NEW_CODE 
-end
-end
+	set -q NX3_REBOOT_TIMEOUT[1]; or set NX3_REBOOT_TIMEOUT 60
+	while true
+		sleep $NX3_REBOOT_TIMEOUT
+		set NX3_NEW_CODE (curl "$NX3_REBOOT_URL?code=$NX3_QUERY_CODE" 2>/dev/null)
+		test "$NX3_OLD_CODE" = "$NX3_NEW_CODE"; or begin;
+			echo "rebooting nx3"
+			virsh reboot nx3
+			set NX3_OLD_CODE $NX3_NEW_CODE 
+		end
+	end
 end
