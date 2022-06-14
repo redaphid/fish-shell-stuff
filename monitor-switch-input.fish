427b1fac746e54d1f8b35c2a9598080a1e0d41bd
redaphid <iam@aaronherres.com>
2022-06-14 11:47:58 -0700

    functions: monitor-switch-input

--- monitor-switch-input.fish
+++ monitor-switch-input.fish
@@ -1,25 +1,24 @@
-function monitor-switch-input
-    set -q MONITOR_INPUT[1]; or begin
-        echo "please set MONITOR_INPUT to the i2c input code you want to switch to (e.g. 15,17)"
-        return
-    end
+function monitor-switch-input --argument monitor
+    set -q monitor[1]; or set -l monitor $MONITOR_INPUT
     while true
         sleep 3
-
+	echo "monitor before: $monitor"
+	set -q monitor[1]; or set -l monitor (monitor-get-control-device)
+	echo "monitor after: $monitor"
         has_das; and begin
             echo "has das"
-            ssh mortal.local ddccontrol -r 0x60 dev:$MONITOR_INPUT 2>/dev/null | grep 15; and continue
-            ssh mortal.local ddccontrol -r 0x60 -w 15 dev:$MONITOR_INPUT
+            ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep 15; and continue
+            ssh mortal.local ddccontrol -r 0x60 -w 15 dev:$monitor
             continue
         end
         ssh mumra.local has_das; and begin
             echo "mumra has_das"
-            ssh mortal.local ddccontrol -r 0x60 dev:$MONITOR_INPUT 2>/dev/null | grep 17; and continue
-            ssh mortal.local ddccontrol -r 0x60 -w 17 dev:$MONITOR_INPUT
+            ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep 17; and continue
+            ssh mortal.local ddccontrol -r 0x60 -w 17 dev:$monitor
             continue
         end
-        ssh mortal.local ddccontrol -r 0x60 dev:$MONITOR_INPUT 2>/dev/null | grep 18; and continue
-        ssh mortal.local ddccontrol -r 0x60 -w 18 dev:$MONITOR_INPUT
+        ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep 18; and continue
+        ssh mortal.local ddccontrol -r 0x60 -w 18 dev:$monitor
 	continue
     end
 
