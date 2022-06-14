e17c40f62d75cf0ffef4a4a77df0886c0bdd559a
redaphid <iam@aaronherres.com>
2022-06-14 10:00:14 -0700

    functions: ros-get-latest

---
+++ ros-get-latest.fish
@@ -0,0 +1,11 @@
+function ros-get-latest
+    set -q save_dir[1]; or set save_dir $ROS_FUNCTION_LOCATION
+    set -q save_dir[1]; or begin
+        echo "I need to know where to save stuff to. try calling this with `save_dir=<directory with the functions> ros-save <functions...>"
+        echo "or setting ROS_FUNCTION_LOCATION as en env var for your system"
+        return 1
+    end
+    pushd $ROS_FUNCTION_LOCATION
+    git pull
+    popd
+end
