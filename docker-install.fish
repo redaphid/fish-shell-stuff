f1cdc0d25044e803c92028eb50bc54790e10e85a
redaphid <iam@aaronherres.com>
2022-06-13 17:45:42 -0700

    functions: docker-install

---
+++ docker-install.fish
@@ -0,0 +1,14 @@
+function docker-install
+ sudo apt-get remove docker docker-engine docker.io containerd runc
+ sudo apt update 
+ sudo apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo  mkdir -p /etc/apt/keyrings
+
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ sudo apt update 
+ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+end
