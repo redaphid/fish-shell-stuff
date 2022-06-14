39d006368d31dff04b3d8f3efc79b8e993bf6c87
knock <knock@knock.local>
2022-06-14 16:24:57 +0000

    functions: zfs-allow-all

--- zfs-allow-all.fish
+++ zfs-allow-all.fish
@@ -1,17 +1,16 @@
-#!/usr/bin/env fish
-function zfs-allow-all --description 'allows <user> all permissions to <dataset>' --argument user dataset
-    #    for line in (zfs allow 2>&1 | grep '\Wproperty\W')
-    #        set perm (echo $line | string replace 'property' '' | string trim)
-    #        set perm2 (echo $line | string replace 'subcommand' '' | string trim)
-    #	echo 'zfs allow -u' $user $perm $dataset
-    #	echo $perm2
-    #        #zfs allow -u $user $perm $dataset
-    #    end
-    #   
-	argparse -i 'a/all' -- $argv
-    for line in (zfs allow 2>&1 | grep '\Wsubcommand\W')
-        set perm (echo $line | string split 'subcommand' | string trim)[1]
-        sudo zfs allow -u $user $perm $dataset
+function zfs-allow-all --description 'allows <user> all permissions to <dataset>' --argument user dataset dry_run
+    argparse -i 'a/all' -- $argv
+    set -q user[1]; and set -q dataset[1]; or begin
+	echo "usage: zfs-allow-all <user> <dataset> <dry_run>"
+	return
+    end
+    set -e found_beginning
+    for line in (zfs allow 2>&1)
+	echo $line | grep -q 'NAME'; and set found_beginning "true"
+	set -q found_beginning[1]; or continue
+	string match -r '^y' $dry_run[1]; and echo "Dry run"
+	echo $line | grep -q '[[:space:]]\(subcommand\|property\)[[:space:]]'; or continue
+	set perm (string split --no-empty ' ' $line)[1]
+	echo zfs allow -r -u $user $perm $dataset
     end
 end
-status is-interactive; or zfs-allow-all $argv
