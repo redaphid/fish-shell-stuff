ca74ce54c1ffd646729240a99b3c9385caba95f4
redaphid <iam@aaronherres.com>
2022-06-14 21:20:25 -0700

    functions: monitor-switch-input

--- monitor-switch-input.fish
+++ monitor-switch-input.fish
@@ -5,18 +5,18 @@ function monitor-switch-input --argument monitor
 	set -q monitor[1]; or set -l monitor (monitor-get-control-device)
         has_das; and begin
             echo "has das"
-            ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 15; and continue
-            ssh mortal.local ddccontrol -r 0x60 -w 15 dev:$monitor
+             ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 15; and continue
+             ddccontrol -r 0x60 -w 15 dev:$monitor
             continue
         end
         ssh mumra.local has_das; and begin
             echo "mumra has_das"
-            ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 17; and continue
-            ssh mortal.local ddccontrol -r 0x60 -w 17 dev:$monitor
+             ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 17; and continue
+             ddccontrol -r 0x60 -w 17 dev:$monitor
             continue
         end
-        ssh mortal.local ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 18; and continue
-        ssh mortal.local ddccontrol -r 0x60 -w 18 dev:$monitor
+         ddccontrol -r 0x60 dev:$monitor 2>/dev/null | grep -q 18; and continue
+         ddccontrol -r 0x60 -w 18 dev:$monitor
 	continue
     end
 
