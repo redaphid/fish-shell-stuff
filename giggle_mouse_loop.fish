3bb8193ac891a901f27eb976814d5a8c2dac30b7
redaphid <iam@aaronherres.com>
2022-07-12 16:30:30 -0700

    functions: giggle_mouse_loop

--- giggle_mouse_loop.fish
+++ giggle_mouse_loop.fish
@@ -2,12 +2,16 @@ function giggle_mouse_loop --argument interval
     set -q interval[1]; or set interval 5000
     set old_mouse_location (xdotool getmouselocation)
     while true
-        set new_mouse_location (xdotool getmouselocation)
-        test "$old_mouse_location" = "$new_mouse_location"; or continue
-        echo "mouse was same. gotta giggle"
-        set old_mouse_location $new_mouse_location
-        giggle_mouse
         sleep $interval
+        set new_mouse_location (xdotool getmouselocation)
+        echo "$old_mouse_location = $new_mouse_location"
+        test "$old_mouse_location" = "$new_mouse_location"; or begin
+            set old_mouse_location $new_mouse_location
+            echo "mouse was not equal"
+            continue
+        end
+        echo "mouse was same. gotta giggle"
+        giggle_mouse
     end
     
 end
