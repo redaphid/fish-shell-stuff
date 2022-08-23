64453cab06574449e764cb610e859c65b5b08347
redaphid <iam@aaronherres.com>
2022-08-23 14:19:46 -0700

    nvm use

--- nvm_use.fish
+++ nvm_use.fish
@@ -1,5 +1,10 @@
 function nvm_use --on-variable PWD
 	test -f .nvmrc; and begin
+		set prev_node_version (node --version)
 		nvm use
+		set new_node_version (node --version)
+		if [ $prev_node_version != $new_node_version ]
+			echo "$old_node_version -> $new_node_version"
+		end
 	end
 end
