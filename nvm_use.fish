e8eeec10b05dc686ed85c5525dd8a510fdc91483
redaphid <iam@aaronherres.com>
2022-08-23 14:24:22 -0700

    nvm use and notify on node changes

--- nvm_use.fish
+++ nvm_use.fish
@@ -1,10 +1,8 @@
 function nvm_use --on-variable PWD
-	test -f .nvmrc; and begin
 		set prev_node_version (node --version)
-		nvm use
+		nvm use --silent
 		set new_node_version (node --version)
 		if [ $prev_node_version != $new_node_version ]
-			echo "$old_node_version -> $new_node_version"
+			echo "$prev_node_version -> $new_node_version"
 		end
-	end
 end
