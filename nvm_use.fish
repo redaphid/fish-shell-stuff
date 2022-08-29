1e429abd6f7eea1006d993cfd07a5aea8e7fed65
redaphid <iam@aaronherres/com>
2022-08-28 20:04:49 -0700

    nvm_use that's isn't so broken

--- nvm_use.fish
+++ nvm_use.fish
@@ -1,9 +1,10 @@
 function nvm_use --on-variable PWD
 		type -q nvm; or return
+		type -q node; or nvm use
 		set prev_node_version (node --version)
 		nvm use --silent
 		set new_node_version (node --version)
 		if [ $prev_node_version != $new_node_version ]
-			echo "$prev_node_version -> $new_node_version"
+				echo "$prev_node_version -> $new_node_version"
 		end
 end
