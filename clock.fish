16f37b17bbe03586e787cc37fe0ae181c4a922ca
redaphid <iam@aaronherres.com>
2022-09-06 15:09:17 -0700

    update

--- clock.fish
+++ clock.fish
@@ -1,10 +1,10 @@
 function clock
     set cf (date '+%s')
-
+    echo $cf
     echo date '+%s' >>~/Documents/clock/$cf
     while true
         sleep 1
-        echo date '+%s' >>~/Documents/clock
+        echo date '+%s' >>~/Documents/clock/$cf
     end
-    trap 'echo date "+%s" >> ~/Documents/clock' INT
+    #trap 'echo date "+%s" >> ~/Documents/clock' INT
 end
