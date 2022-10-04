c3b33a572091c4b23442659a80a1db9d274dcb0a
redaphid <iam@aaronherres.com>
2022-10-04 12:46:54 -0700

    allow gpo to save upstream config

--- gpo.fish
+++ gpo.fish
@@ -1,9 +1,10 @@
-function gpo --description 'Pushes current branch to the specified origin. Useful for pushing to multiple repos' --argument repo_name
-        set -q repo_name[1]; or set repo_name $GPO_DEFAULT_REPO
-        set -q repo_name[1]; or begin
-            echo "Must specify a repo name, or set GPO_DEFAULT_REPO to something (likely origin)"
-            echo "e.g. gpo redaphid"
+function gpo --description 'Pushes current branch to the specified origin. Useful for pushing to multiple repos' --argument repo_name save
+    set -q repo_name[1]; or set repo_name $GPO_DEFAULT_REPO
+    set -q repo_name[1]; or begin
+        echo "Must specify a repo name, or set GPO_DEFAULT_REPO to something (likely origin)"
+        echo "e.g. gpo redaphid"
         return
     end
-    git push $repo_name (git branch --show-current)
+    set -q save[1]; and set extra_options set extra_options ('--set-upstream $repo_name')
+    git push $extra_potions $repo_name (git branch --show-current)
 end
