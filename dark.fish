dd0df9f99a8388fcfe32042823b4ec5f80734bee
redaphid <iam@aaronherres/com>
2023-03-17 16:24:28 -0700

    dark and light functions for alacritty

--- dark.fish
+++ dark.fish
@@ -1,5 +1,5 @@
 function dark
-    set -q ALACRITTY_DARK_THEME; or set ALACRITTY_DARK_THEME github_light_default
+    set -q ALACRITTY_DARK_THEME; or set ALACRITTY_DARK_THEME github_dark_default
     rm ~/.config/alacritty/current_theme.yaml
-    echo ln -s ~/.config/alacritty/themes/$ALACRITTY_DARK_THEME.yaml ~/.config/alacritty/current-theme.yaml
+    ln -s ~/.config/alacritty/themes/$ALACRITTY_DARK_THEME.yaml ~/.config/alacritty/current_theme.yaml
 end
