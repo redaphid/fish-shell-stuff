3515ef73795916cdd9c600a05b4237196ad1d8a9
redaphid <iam@aaronherres/com>
2023-07-07 02:21:20 -0700

    init project requires project type

--- init-project.fish
+++ init-project.fish
@@ -1,17 +1,15 @@
 #!/usr/bin/env fish
-function init-project -a repo_name -a private -a project_type -a source_repo -a source_tag
+function init-project -a project_type -a repo_name -a private -a source_repo -a source_tag
   set -q repo_name[1]; or begin
-      echo "Usage: init-project <repo_name> [private] [source_repo] [source_tag]"
+      echo "Usage: init-project <project_type> <repo_name> [private] [source_repo] [source_tag]"
       return 1
   end
   set -q private[1]; or set private "false"
   set -q project_type[1]; or set project_type $INIT_PROJECT_TYPE
   set -q project_type[1]; or set project_type "typescript"
-  set -q source_repo[1]; or set source_repo $INIT_PROJECT_SOURCE_REPO$project_type
+  set -q source_repo[1]; or set source_repo $INIT_PROJECT_SOURCE_REPO_$project_type
   set -q source_repo[1]; or set source_repo "git@github.com:redaphid/$project_type-project-template.git"
-  echo $source_repo
-  return 1
-  set -q source_tag[1]; or set source_tag $INIT_PROJECT_SOURCE_TAG
+  set -q source_tag[1]; or set source_tag $project_type$INIT_PROJECT_SOURCE_TAG
   set -q source_tag[1]; or set source_tag "boilerplate"
 
   set gh_repo_scope_flag (test $private = "true"; and echo "--private"; or echo "--public")
