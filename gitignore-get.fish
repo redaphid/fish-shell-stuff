962138a395865c082b601e45694c5122817e6d87
redaphid <iam@aaronherres.com>
2023-08-17 17:26:38 -0700

    functions: gitignore-get

--- gitignore-get.fish
+++ gitignore-get.fish
@@ -1,3 +1,3 @@
 function gitignore-get
-  curl https://gist.githubusercontent.com/redaphid/a0617db69f81d45fa9c62cf3f6995cbf/raw/.gitignore
+    curl https://gist.githubusercontent.com/redaphid/a0617db69f81d45fa9c62cf3f6995cbf/raw/0bddc74256589c73dc8041c80a616ed067df934e/.gitignore
 end
