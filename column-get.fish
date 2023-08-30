7910242916ba52ce5164c9092f86af43b1b007ee
redaphid <iam@aaronherres.com>
2023-08-30 15:49:56 -0700

    functions: column-get

--- column-get.fish
+++ column-get.fish
@@ -3,7 +3,7 @@ set -q column_number[1]; or set column_number $COLUMN_NUMBER;
 set -q column_number[1]; or begin
 echo "error: column number must be specified as the first parameter, or as the environment variable COLUMN_NUMBER"
 end
-while read -l line
-tr -s '[:blank:]' '\t' | cut -f$column_number  -
-end
+  while read -l line
+    echo $line | tr -s '[:blank:]' '\t' | cut -f$column_number
+  end
 end
