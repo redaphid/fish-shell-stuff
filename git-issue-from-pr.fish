38ff54f9afe94d6ccbd593a866b1d778f9f1422b
redaphid <iam@aaronherres.com>
2023-08-30 17:40:05 -0700

    functions: git-issue-from-pr

--- git-issue-from-pr.fish
+++ git-issue-from-pr.fish
@@ -1,6 +1,4 @@
-#!/usr/bin/env fish
 function git-issue-from-pr
 set issue  (gh pr view --json='body' | jq -r '.body' | string split "Closes #")[3]
 echo $issue
 end
-status is-interactive; or 'git-issue-from-pr'  $argv
