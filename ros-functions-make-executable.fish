c369f2345a0e863cc686b108793e5ab26bde9e8c
redaphid <iam@aaronherres.com>
2023-08-30 19:10:06 -0700

    functions: ros-functions-make-executable

--- ros-functions-make-executable.fish
+++ ros-functions-make-executable.fish
@@ -3,7 +3,7 @@ function ros-functions-make-executable
     set file_name $argv[1]
 
     # Extract the file name without the .fish prefix
-    set base_name (string replace -r '\.fish$' '' $file_name)
+    set base_name (string replace -ra '\.fish$' '' $file_name)
 
     # Read the first line of the file
     set first_line (head -n 1 $file_name)
@@ -13,16 +13,16 @@ function ros-functions-make-executable
 
     # Check if the first line is missing, add it
     if not contains -- "$first_line" "#!/usr/bin/env fish"
-        echo "#!/usr/bin/env fish" > $file_name.tmp
-        cat $file_name >> $file_name.tmp
+        echo "#!/usr/bin/env fish" >$file_name.tmp
+        cat $file_name >>$file_name.tmp
         mv $file_name.tmp $file_name
         echo "Added shebang to '$file_name'."
     end
 
     # Check if the last line is missing, add it
-    set expected_last_line "status is-interactive; or '$base_name'  \$argv"
+    set expected_last_line "status is-interactive; or $base_name \$argv"
     if not contains -- "$last_line" "$expected_last_line"
-        echo "$expected_last_line" >> $file_name
+        echo "$expected_last_line" >>$file_name
         echo "Added last line to '$file_name'."
     end
 
