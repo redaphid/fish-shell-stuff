2feb71463b77563b9b160a120a1795d556ce4aaf
redaphid <iam@aaronherres.com>
2023-08-30 17:39:25 -0700

    functions: docker-remove-all-volumes

--- docker-remove-all-volumes.fish
+++ docker-remove-all-volumes.fish
@@ -1,7 +1,5 @@
-#!/usr/bin/env fish
 function docker-remove-all-volumes
 docker-compose down;
 docker rm -f (docker ps -a -q);
 docker volume rm (docker volume ls -q);
 end
-status is-interactive; or 'docker-remove-all-volumes'  $argv
