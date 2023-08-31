6932eec9d71767c147c008cf8a44326cc1b8e7b3
redaphid <iam@aaronherres.com>
2023-08-30 17:17:44 -0700

    functions: decho

--- decho.fish
+++ decho.fish
@@ -1,11 +1,9 @@
-#!/usr/bin/env fish
 function decho --wraps=echo --description 'echo to stderr'
-   isatty stdin; or begin
+    isatty stdin; or begin
         cat - | while read -l line
-        set -a args $line
+            echo $argv $line 1>&2
         end
-  end; 
-        set -a args $argv
-        echo $args 1>&2
+        return
+    end
+    echo $argv 1>&2
 end
-status is-interactive; or 'decho'  $argv
