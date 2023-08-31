8ba27adcae842ff5b8e5d26f049f088ff0ba3697
redaphid <iam@aaronherres.com>
2023-08-30 17:40:00 -0700

    functions: git-branch-name-from-issue

--- git-branch-name-from-issue.fish
+++ git-branch-name-from-issue.fish
@@ -1,7 +1,5 @@
-#!/usr/bin/env fish
 function git-branch-name-from-issue --argument issue
     set issue_title (gh issue view $issue --json title --jq '.title')
     set branch_name (echo $issue_title | string replace --all --regex '^\W+|\W+$' '' | string replace --all --regex '\W+' '-' | string lower)
     echo $branch_name
 end
-status is-interactive; or 'git-branch-name-from-issue'  $argv
