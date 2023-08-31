46a5471f3739b69358f48632462bc2250330f77f
redaphid <iam@aaronherres.com>
2023-08-30 17:39:34 -0700

    functions: efi-remove-inactive

--- efi-remove-inactive.fish
+++ efi-remove-inactive.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function efi-remove-inactive
 efibootmgr | grep -v '*' | string replace 'Boot' '' | awk '{print $1}' | xargs -i efibootmgr -b {}  --delete-bootnum
 end
-status is-interactive; or 'efi-remove-inactive'  $argv
