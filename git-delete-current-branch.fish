339a57da1427b931aae352de3d08931043cdeaff
redaphid <iam@aaronherres.com>
2023-08-30 17:40:01 -0700

    functions: git-delete-current-branch

--- git-delete-current-branch.fish
+++ git-delete-current-branch.fish
@@ -1,7 +1,5 @@
-#!/usr/bin/env fish
 function git-delete-current-branch
 	set current_branch (git branch --show-current)
 	git checkout main
 	git branch -D $current_branch
 end
-status is-interactive; or 'git-delete-current-branch'  $argv
