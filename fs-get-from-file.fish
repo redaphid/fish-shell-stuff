40c0411a61fd1caa9c12b3298e8ba84968c81778
redaphid <iam@aaronherres.com>
2023-08-30 17:39:48 -0700

    functions: fs-get-from-file

--- fs-get-from-file.fish
+++ fs-get-from-file.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function fs-get-from-file --argument FILE
  string escape --style=var (echo (df $FILE)[2] | string split ' ')[1]; 
 end
-status is-interactive; or 'fs-get-from-file'  $argv
