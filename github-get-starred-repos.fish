3a22b339cec93a300e9185f51b29fe612fb72db9
redaphid <iam@aaronherres.com>
2023-08-30 17:40:10 -0700

    functions: github-get-starred-repos

--- github-get-starred-repos.fish
+++ github-get-starred-repos.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function github-get-starred-repos
  gh api user/starred --template '{{range .}}{{.full_name|color "yellow"}} ({{timeago .updated_at}}){{"\n"}}{{end}}'; 
 end
-status is-interactive; or 'github-get-starred-repos'  $argv
