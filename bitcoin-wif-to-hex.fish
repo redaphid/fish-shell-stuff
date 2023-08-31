3b2bec074c835bf7743a28de27a456654681ef4b
redaphid <iam@aaronherres.com>
2023-08-30 17:39:02 -0700

    functions: bitcoin-wif-to-hex

--- bitcoin-wif-to-hex.fish
+++ bitcoin-wif-to-hex.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 function bitcoin-wif-to-hex --argument addr
 	set decoded (echo $addr | base58 -d | xxd -u -g 0| string split ':')
 	set hex_addr (echo $decoded[2] | string split ' ')[2]
@@ -6,4 +5,3 @@ function bitcoin-wif-to-hex --argument addr
 	set -a hex_addr (echo $decoded[6] | string split ' ')[2]
 	echo (echo $hex_addr | string collect | string replace -a ' ' '')
 end
-status is-interactive; or 'bitcoin-wif-to-hex'  $argv
