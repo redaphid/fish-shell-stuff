b1a2b513d006c6b7fe17153471b175350453e0a6
redaphid <iam@aaronherres.com>
2023-08-30 17:39:39 -0700

    functions: filename-sanitizer

--- filename-sanitizer.fish
+++ filename-sanitizer.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function filename-sanitizer
  for i in $argv; string replace -a ' ' '-' $i | string lower | string replace -ar '\-{1,}' '-' | string replace -ar '\(.*$' '.mp3' | string replace -ar '\-\(.*(.*$)' '$1.$2' | string replace -a \' '' | string replace -r '\-\.(.*)' '.$1'; end;
 end
-status is-interactive; or 'filename-sanitizer'  $argv
