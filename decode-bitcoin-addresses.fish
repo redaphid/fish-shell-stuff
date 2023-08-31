47a0f5b3015bf0a9a1f40558973223a2e2e09652
redaphid <iam@aaronherres.com>
2023-08-30 17:39:17 -0700

    functions: decode-bitcoin-addresses

--- decode-bitcoin-addresses.fish
+++ decode-bitcoin-addresses.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 function decode-bitcoin-addresses
 	for a in (cat /home/redaphid/bitcoin-search/sanitized-keys.txt)              04:13:00
                         set decoded (echo $a | base58 -d | xxd -l 16 | string split ':')
@@ -6,4 +5,3 @@ function decode-bitcoin-addresses
 			echo $decoded[2]
         end
 end
-status is-interactive; or 'decode-bitcoin-addresses'  $argv
