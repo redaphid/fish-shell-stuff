c2fe1354e86410509962b8829e9eca3714f1021a
redaphid <iam@aaronherres.com>
2023-08-30 17:40:03 -0700

    functions: git-first-commit-of-branch

--- git-first-commit-of-branch.fish
+++ git-first-commit-of-branch.fish
@@ -1,6 +1,4 @@
-#!/usr/bin/env fish
 function git-first-commit-of-branch
     set commit_with_hash (git log main.. --oneline | tail -1)
     string sub $commit_with_hash --start=9
 end
-status is-interactive; or 'git-first-commit-of-branch'  $argv
