e085b4daed758ac2d97c48c0df4c441a918fc5f8
redaphid <iam@aaronherres.com>
2023-08-30 17:39:00 -0700

    functions: bitcoin-unique-in-dir

--- bitcoin-unique-in-dir.fish
+++ bitcoin-unique-in-dir.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function bitcoin-unique-in-dir --description 'runs bitcoin-find-in-dir and prints only unique addresses' --argument dir
  bitcoin-find-in-dir $dir | sort | uniq;
 end
-status is-interactive; or 'bitcoin-unique-in-dir'  $argv
