468eff38a29f4a6fc1c37a0f0240edc432579a38
redaphid <iam@aaronherres.com>
2023-08-30 17:39:07 -0700

    functions: copy-files-fast

--- copy-files-fast.fish
+++ copy-files-fast.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function copy-files-fast --description 'uses rsync to quickly copy from <src> to <dst>' --argument src dst
     rsync -avhW --no-compress --info=progress2 $src/ $dst
 end
-status is-interactive; or 'copy-files-fast'  $argv
