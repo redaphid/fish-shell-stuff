b9de61ef5d8dda4c96f5bf45346f24c13b17f3dc
redaphid <iam@aaronherres.com>
2023-08-30 17:39:47 -0700

    functions: force-ethernet-to-1000

--- force-ethernet-to-1000.fish
+++ force-ethernet-to-1000.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/env fish
 function force-ethernet-to-1000
  sudo /usr/sbin/ethtool -s (network-names) speed 1000 duplex full autoneg off
 end
-status is-interactive; or 'force-ethernet-to-1000'  $argv
