d2aaf1070d7cd0df4964fc69ce0ca972f29e8a2d
redaphid <iam@aaronherres.com>
2023-08-30 17:40:08 -0700

    functions: git-protocol-labs-merge-pr

--- git-protocol-labs-merge-pr.fish
+++ git-protocol-labs-merge-pr.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 function git-protocol-labs-merge-pr
 	set branch_name (git branch --show-current)
 	echo $branch_name
@@ -6,4 +5,3 @@ function git-protocol-labs-merge-pr
 	set commit_msg (gh pr view $branch_name --json "body" --jq ".body")
 	gh pr merge $branch_name --squash --body $commit_msg
 end
-status is-interactive; or 'git-protocol-labs-merge-pr'  $argv
