7d848d5379ed59eb23b381231e062bc13e88d3f4
redaphid <iam@aaronherres.com>
2023-08-30 17:40:11 -0700

    functions: gpo

--- gpo.fish
+++ gpo.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 function gpo --description 'Pushes current branch to the specified origin. Useful for pushing to multiple repos' --argument repo_name save
     set -q repo_name[1]; or set repo_name $GPO_DEFAULT_REPO
     set -q repo_name[1]; or begin
@@ -12,4 +11,3 @@ function gpo --description 'Pushes current branch to the specified origin. Usefu
     end
     git push $repo_name (git branch --show-current)
 end
-status is-interactive; or 'gpo'  $argv
