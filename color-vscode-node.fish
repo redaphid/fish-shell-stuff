e8b91af0bbf87a7d8a7ca09ec42a28338f69f9d8
redaphid <iam@aaronherres.com>
2023-09-11 19:02:00 -0700

    functions: color-vscode-node

--- color-vscode-node.fish
+++ color-vscode-node.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
-function color-vscode-node
-
-set background (cat package.json | jq '.name' | md5sum | string sub -s 1 -l 6  | pastel darken 0.3 | pastel format hex)
-set foreground (pastel textcolor $background | pastel format hex) 
-echo "background": $background
-echo "foreground": $foreground
+function color-vscode-node --argument darken_amount
+    set -q darken_amount[1]; or set darken_amount 0.1
+    set background (cat package.json | jq '.name' | md5sum | string sub -s 1 -l 6  | pastel darken $darken_amount | pastel format hex)
+    set foreground (pastel textcolor $background | pastel format hex)
+    echo "background": $background
+    echo "foreground": $foreground
 end
 status is-interactive; or color-vscode-node $argv
