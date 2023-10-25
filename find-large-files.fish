fbb8f18d5bfd40e5ff3028063ccba8f4b38e77e0
redaphid <iam@aaronherres/com>
2023-10-24 22:23:58 -0700

    functions: find-large-files

--- find-large-files.fish
+++ find-large-files.fish
@@ -1,4 +1,4 @@
-function find-large-files -a dir
-set -q dir; or set dir "."
-ncdu --exclude "/big" --exclude "/tmp" --exclude "/proc" --exclude "/var/lib/docker" --exclude "/mnt" / $dir
+function find-large-files --argument dir
+    set -q dir; or set dir "."
+    ncdu --exclude /big --exclude /tmp --exclude /proc --exclude /var/lib/docker --exclude /mnt --exclude / $dir
 end
