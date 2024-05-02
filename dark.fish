706ef49b60926d7810bd1572d7b41e0d64756279
redaphid <iam@aaronherres.com>
2024-05-02 15:47:01 -0700

    fix light and dark so they work w/toml files for alacritty

--- dark.fish
+++ dark.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 function dark
     set -q ALACRITTY_DARK_THEME; or set ALACRITTY_DARK_THEME github_dark_default
-    rm ~/.config/alacritty/current_theme.yaml
-    ln -s ~/.config/alacritty/themes/$ALACRITTY_DARK_THEME.yaml ~/.config/alacritty/current_theme.yaml
+    rm ~/.config/alacritty/current_theme.toml
+    ln -s ~/.config/alacritty/themes/$ALACRITTY_DARK_THEME.toml ~/.config/alacritty/current_theme.toml
+    touch ~/.config/alacritty/alacritty.toml
 end
 status is-interactive; or dark $argv
--- light.fish
+++ light.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 function light
-    set -q ALACRITTY_LIGHT_THEME; or set ALACRITTY_LIGHT_THEME github_light_default
-    rm ~/.config/alacritty/current_theme.yaml
-    ln -s ~/.config/alacritty/themes/$ALACRITTY_LIGHT_THEME.yaml ~/.config/alacritty/current_theme.yaml
-    touch ~/.config/alacritty/alacritty.yml
+    set -q ALACRITTY_LIGHT_THEME; or set ALACRITTY_LIGHT_THEME github_light
+    rm ~/.config/alacritty/current_theme.toml
+    ln -s ~/.config/alacritty/themes/$ALACRITTY_LIGHT_THEME.toml ~/.config/alacritty/current_theme.toml
+    touch ~/.config/alacritty/alacritty.toml
 end
 status is-interactive; or light $argv
