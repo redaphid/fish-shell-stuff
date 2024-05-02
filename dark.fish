cd4e056626dab5f7990334b66d68c5437b51ac51
redaphid <iam@aaronherres.com>
2024-05-02 15:49:59 -0700

    dark and light mode changes system themes

--- dark.fish
+++ dark.fish
@@ -4,5 +4,9 @@ function dark
     rm ~/.config/alacritty/current_theme.toml
     ln -s ~/.config/alacritty/themes/$ALACRITTY_DARK_THEME.toml ~/.config/alacritty/current_theme.toml
     touch ~/.config/alacritty/alacritty.toml
+    if string match -q "Darwin" -- (uname)
+        # Set the system theme to dark
+        osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+    end
 end
 status is-interactive; or dark $argv
--- light.fish
+++ light.fish
@@ -4,5 +4,10 @@ function light
     rm ~/.config/alacritty/current_theme.toml
     ln -s ~/.config/alacritty/themes/$ALACRITTY_LIGHT_THEME.toml ~/.config/alacritty/current_theme.toml
     touch ~/.config/alacritty/alacritty.toml
+    if string match -q "Darwin" -- (uname)
+        # Set the system theme to light
+        osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'
+        echo "System theme set to light mode."
+    end
 end
 status is-interactive; or light $argv
