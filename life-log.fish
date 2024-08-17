1c260eaf3aa1ffe6e3667f12825fb97fb75a2fc7
redaphid <iam@aaronherres/com>
2024-08-16 17:19:19 -0700

    life log

--- life-log.fish
+++ life-log.fish
@@ -1,14 +1,7 @@
-function life-log
-    set -l value $argv
-    set -l service_url "https://life-log.loqwai.workers.dev" # Replace with your actual service URL
-    set -l auth_token $LIFE_LOG_TOKEN
-
-    # Create JSON payload
-    set -l json_payload (echo '{"value": "'$value'"}')
-
-    # Use curl to POST the log event
-    curl -X POST $service_url \
-        -H "Content-Type: application/json" \
-        -H "Authorization: $auth_token" \
-        -d $json_payload
+function life-log --argument value
+    if test -z "$value"
+        http -b $LIFE_LOG_URL authorization:$LIFE_LOG_TOKEN test:$LIFE_LOG_TEST
+    else
+        http -b $LIFE_LOG_URL authorization:$LIFE_LOG_TOKEN test:$LIFE_LOG_TEST value=$value
+    end
 end
