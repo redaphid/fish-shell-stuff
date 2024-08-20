583b35a9636f63eb9f1abbf96215a209808bd0ec
redaphid <iam@aaronherres.com>
2024-08-20 14:06:01 -0700

    venv

--- venv_use.fish
+++ venv_use.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 function venv_use --on-variable PWD
-        if test -d venv/
-            source venv/bin/activate.fish            
+        if test -d .venv/
+            source .venv/bin/activate.fish            
         end
 end
 status is-interactive; or venv_use $argv
