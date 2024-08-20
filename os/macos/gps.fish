ac4f39c149dcbfa9ce477e557b6672f988cd7f2f
redaphid <iam@aaronherres.com>
2024-08-20 11:32:34 -0700

    gps prints a hint when things go wrong

--- os/macos/gps.fish
+++ os/macos/gps.fish
@@ -1,3 +1,3 @@
 function gps
-http --body http://localhost:8000/current_location
+http --body http://localhost:8000/current_location; or decho "Project can be found here: https://github.com/RhetTbull/locationator"
 end
