e549a88cc6e630421b8984cd53efb81d5a623af4
redaphid <iam@aaronherres.com>
2024-09-03 15:19:00 -0700

    functions: ptouch

--- ptouch.fish
+++ ptouch.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 function ptouch --wraps=touch
-echo mkdir -p (dirname $argv[1])
-echo touch $argv
+mkdir -p (dirname $argv[1])
+touch $argv
 end
 status is-interactive; or ptouch $argv
