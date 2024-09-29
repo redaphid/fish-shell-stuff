c97e4027aa336d4a28403375ed77275cd7d46342
knock <knock@knock.local>
2024-09-29 03:43:37 +0000

    remove gitignore-get. We're using cli-tools from now on!

--- gitignore-get.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-function gitignore-get
-    curl https://raw.githubusercontent.com/loqwai/cli-tools/refs/heads/main/.gitignore
-end
-status is-interactive; or gitignore-get $argv
