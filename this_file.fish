26853373ff29da6d0a19198995280923c144fd3d
redaphid <iam@aaronherres/com>
2024-09-29 00:56:33 -0700

    functions: this_file

--- this_file.fish
+++ this_file.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 function this_file
-    echo (realpath (dirname (status current-filename)))
+    echo (realpath (status current-filename))
 end
 status is-interactive; or this_file $argv
