222547299422671ee41c61f19ba455b1795bc25f
redaphid <iam@aaronherres/com>
2025-11-21 17:57:19 -0700

    Suppress pnpm env output during shell initialization

--- pnpm-nvmrc.fish
+++ pnpm-nvmrc.fish
@@ -1,6 +1,6 @@
 function __check_nvmrc --on-variable PWD
     if test -f .nvmrc
         set -l node_version (cat .nvmrc)
-        pnpm env use $node_version
+        pnpm env use $node_version &>/dev/null
     end
 end
